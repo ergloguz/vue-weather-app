ac7c172be9bfe55e8e76cf423266270b4f21ac46
Oğuz Ergül <erguloguz@hotmail.com>
2020-08-23 18:52:27 +0300

    son değişiklikler yapıldı

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-
     <router-view/>
     <Footer/>
   </div>
@@ -12,13 +11,11 @@
 
 </style>
 <script>
-import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 
 export default {
   name:"App",
   components: {
-    Navigation,
     Footer
   }
 }
